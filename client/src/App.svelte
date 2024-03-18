c065f7cfcc59161bcfdc5ad7ec54846b03589fda
Samsonium <tdesu@vk.com>
2024-03-18 16:35:13 +0300

    Add init to App.svelte component

---
+++ client/src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import Graphics from './game/graphics';
+    import Keyboard from './game/keyboard';
+    import Network from './game/network';
+
+    let canvas: HTMLCanvasElement;
+    onMount(() => {
+        const n = new Network();
+        const g = new Graphics(canvas);
+        const kb = new Keyboard();
+
+        g.setupScene();
+        g.begin();
+
+        return () => {
+            n.destroy();
+            g.destroy();
+            kb.destroy();
+        }
+    });
+</script>
+
+<canvas bind:this={canvas}></canvas>
+
+<style>
+    canvas {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
