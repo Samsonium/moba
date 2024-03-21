18af649339f61a40c851a64114ad3e001b0fdb1e
Samsonium <tdesu@vk.com>
2024-03-21 10:08:12 +0300

    [client]: update asset list

--- client/src/lib/asset-list.ts
+++ client/src/lib/asset-list.ts
@@ -2,5 +2,6 @@ import AssetLoader from '../game/assets/AssetLoader';
 
 export const assetList = {
     lobbyMap: AssetLoader.createGLTF('/objects/lobby_map.glb'),
-    lobbyNavmesh: AssetLoader.createGLTF('/objects/lobby_navmesh.gltf')
+    lobbyNavmesh: AssetLoader.createGLTF('/objects/lobby_navmesh.gltf'),
+    yBot: AssetLoader.createFBX('/objects/y_bot.fbx'),
 };
