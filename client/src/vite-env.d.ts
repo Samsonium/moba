dae27dafb0128d2940cb175615efda8750cc39b3
Samsonium <tdesu@vk.com>
2024-03-21 17:11:29 +0300

    [client]: add types for libs without typings

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,24 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare module 'three-pathfinding' {
+    import { Object3D, type Vector3, type BufferGeometry } from 'three';
+
+    export class Pathfinding {
+        public setZoneData(zoneName: string, zoneData: any): void;
+        public getGroup(zone: string, position: Vector3): any;
+        public getClosestNode(position: Vector3, zone: string, group: any): {centroid: Vector3};
+        public findPath(start: Vector3, end: Vector3, zone: string, group: any): Vector3[];
+        public static createZone(geom: BufferGeometry): any;
+    }
+
+    export class PathfindingHelper extends Object3D {}
+}
+
+declare module 'three/examples/jsm/loaders/GLTFLoader' {
+    import type { Group } from 'three';
+
+    export interface GLTF {
+        scene: Group;
+    }
+}
