2f4e0e7a578c508e0f00fbfa9e31be349358223c
samsonium <tdesu@vk.com>
2024-03-22 18:30:11 +0300

    [server]: update yBot asset type

--- client/src/lib/asset-list.ts
+++ client/src/lib/asset-list.ts
@@ -3,5 +3,5 @@ import AssetLoader from '../game/assets/AssetLoader';
 export const assetList = {
     lobbyMap: AssetLoader.createGLTF('/objects/lobby_map.glb'),
     lobbyNavmesh: AssetLoader.createGLTF('/objects/lobby_navmesh.gltf'),
-    yBot: AssetLoader.createFBX('/objects/y_bot.fbx'),
+    yBot: AssetLoader.createGLTF('/objects/y_bot.glb'),
 };
