b939b36e26cf6c238599c2013544a2906780d9f9
samsonium <tdesu@vk.com>
2024-03-22 23:27:08 +0300

    [client]: add idle and run animations

--- client/src/lib/asset-list.ts
+++ client/src/lib/asset-list.ts
@@ -3,5 +3,7 @@ import AssetLoader from '../game/assets/AssetLoader';
 export const assetList = {
     lobbyMap: AssetLoader.createGLTF('/objects/lobby_map.glb'),
     lobbyNavmesh: AssetLoader.createGLTF('/objects/lobby_navmesh.gltf'),
-    yBot: AssetLoader.createGLTF('/objects/y_bot.glb'),
+    yBot: AssetLoader.createFBX('/objects/y_bot.fbx'),
+    yBot_aIdle: AssetLoader.createFBX('/animations/y_bot_idle.fbx'),
+    yBot_aRun: AssetLoader.createFBX('/animations/y_bot_run.fbx')
 };
